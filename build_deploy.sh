7f563938692e2469281de42373c1270c14cd60bf
Chris Kyrouac <fijshion@users.noreply.github.com>
2023-03-17 10:02:49 -0400

    Tag latest image (#5)

--- build_deploy.sh
+++ build_deploy.sh
@@ -15,4 +15,6 @@ DOCKER_CONF="$PWD/.docker"
 mkdir -p "$DOCKER_CONF"
 docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 docker --config="$DOCKER_CONF" build -f src/main/docker/Dockerfile.multistage -t "${IMAGE}:${IMAGE_TAG}" .
+docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:latest"
 docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
+docker --config="$DOCKER_CONF" push "${IMAGE}:latest"
